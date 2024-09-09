e6ac366c282e88a997c020a494732833d3c1780a
fahry_r_siregar <fahrysir@192.168.1.13>
2024-09-09 17:50:41 +0700

    component, binding, directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = '101-angular';
+  isTitleChanged: boolean = false;
+
+  changeTitle() {
+    this.title = '102-angular';
+    this.isTitleChanged = true;
+  }
+
+  revertTitle() {
+    this.title = '101-angular';
+    this.isTitleChanged = false;
+  }
 }
