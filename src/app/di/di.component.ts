fcb440b3467fb6cf8407c59d40aef140f5f355b5
fahry_r_siregar <fahrysir@PB92063-24MB.local>
2024-09-17 13:52:36 +0700

    simple DI

--- src/app/di/di.component.ts
+++ src/app/di/di.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-di',
   standalone: true,
   imports: [],
   templateUrl: './di.component.html',
-  styleUrl: './di.component.scss'
+  styleUrl: './di.component.scss',
 })
 export class DiComponent {
+  display = '';
+  // dataService = inject(DataService);
 
+  // constructor() {
+  //   this.display = this.dataService.getCars().join(' ⭐️ ');
+  // }
+  constructor(private _dataService: DataService) {
+    this.display = this._dataService.getCars().join(' ⭐️ ');
+  }
 }
